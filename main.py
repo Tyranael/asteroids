89eca6abcdb6832ca0e4b76d09d6ab1998cd2144
Shaun Chowthee <sn.chowthee@gmail.com>
2026-08-19 14:47:04 +0200

    initialized base class for objects and player object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,8 +9,15 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+#Initializing the game elements
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0.0
+player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
+player.draw(screen)
+
+#Helpers
 def pygame_queue_event():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -21,7 +29,7 @@ while True:
     pygame_queue_event()
     screen.fill("black")
     pygame.display.flip()
-
+    dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
