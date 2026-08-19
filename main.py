24ea0bf4d44dfbce64714ad347278cb9fc89f1f0
Shaun Chowthee <sn.chowthee@gmail.com>
2026-08-19 16:09:12 +0200

    feature: now asteroids collision with player are detected and logged

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
-from logger import log_state
+from logger import log_state, log_event
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
 from asteroids import Asteroid
 from asteroidfield import AsteroidField
+import sys
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -49,6 +50,11 @@ while True:
     #Updating data and position
     pygame.display.flip()
     updatable.update(dt)
+    for ast in asteroids:
+        if ast.collides_with(player):
+            log_event("player_hit")
+            print("Game over!")
+            sys.exit(1)
     dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
